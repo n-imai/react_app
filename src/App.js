87f50f608bbada0fabea1d81e711c501e44067e2
Nobuo Imai <nobuo.imai.ni@gmail.com>
2020-04-29 18:01:36 +0900

    Add Interval

--- src/App.js
+++ src/App.js
@@ -10,11 +10,21 @@ class App extends Component {
     padding: "5px",
     borderBottom: "2px solid #900"
   }
+  btnStyle = {
+    fontSize: "20px",
+    padding: "0px 10px"
+  }
+
   constructor(props){
     super(props);
     this.state = {
       msg: 'Hello Component .',
     };
+    let timer = setInterval(() => {
+      this.setState((state) => ({
+        msg: state.msg + "!"
+      }));
+    },10000);
   }
   render(){
     return <div>
