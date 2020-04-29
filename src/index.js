21b6f53ac7dbb567db4acb43a78961677a7d7215
Nobuo Imai <nobuo.imai.ni@gmail.com>
2020-04-29 17:47:44 +0900

    Use state

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <App />, document.getElementById('root')
+  <App msg="Hello App." />, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
