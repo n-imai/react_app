74c377ec31a85584607f10191fd7951ef2c2f634
Nobuo Imai <nobuo.imai.ni@gmail.com>
2020-05-03 13:17:54 +0900

    Use redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore,combineReducres} from 'redux';
+import {provider, Provider} from 'react-redux';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+let state_value = {
+  counter:0,
+  message:"COUNTER",
+}
+
+function counter(state = state_value ,action){
+  switch (action.type){
+    case 'INCREMENT':
+      return {
+        counter:state.counter + 1,
+        message:"INCREMENT",
+      }
+      case 'DECREMENT':
+        return {
+          counter:state.counter - 1,
+          message:"DECREMENT",
+        }
+      default:
+        return state;
+  }
+}
+
+let store = createStore(counter);
 
 ReactDOM.render(
-  <App />, document.getElementById('root')
+  <Provider store={store}>
+    <App /></Provider>,
+     document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
